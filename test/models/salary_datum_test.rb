b7561d72a769cb103ab52010b21270fb1a54d82a
Dan Moore <bitbucket@mooreds.com>
2019-02-10 21:52:02 +0000

    add in test

--- test/models/salary_datum_test.rb
+++ test/models/salary_datum_test.rb
@@ -4,7 +4,16 @@ class SalaryDatumTest < ActiveSupport::TestCase
   test "default salary datum valid" do
     assert true, build(:salary_datum).valid?
   end
-  test "salary datum invalid with high lower" do
-    assert true, build(:salary_datum).valid?
+  test "salary datum invalid with high lower than low" do
+    assert true, build(:salary_datum, high: 1, low: 2).invalid?
+  end
+  test "salary datum invalid with nil email" do
+    assert true, build(:salary_datum, email: nil).invalid?
+  end
+  test "salary datum invalid with nil linkguid" do
+    assert true, build(:salary_datum, linkguid: nil).invalid?
+  end
+  test "salary datum invalid with nil linkguid" do
+    assert true, build(:salary_datum, linkguid: nil).invalid?
   end
 end
