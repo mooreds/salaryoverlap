415faad9ef65679e28c88c9d9e272950522c7910
Dan Moore <bitbucket@mooreds.com>
2019-02-10 21:53:38 +0000

    bring in annotate gem for better dev envt

--- app/models/salary_datum.rb
+++ app/models/salary_datum.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: salary_data
+#
+#  id          :bigint(8)        not null, primary key
+#  email       :string
+#  linkguid    :string
+#  low         :integer
+#  high        :integer
+#  is_employer :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class SalaryDatum < ApplicationRecord
 end
--- test/models/salary_datum_test.rb
+++ test/models/salary_datum_test.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: salary_data
+#
+#  id          :bigint(8)        not null, primary key
+#  email       :string
+#  linkguid    :string
+#  low         :integer
+#  high        :integer
+#  is_employer :boolean
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class SalaryDatumTest < ActiveSupport::TestCase
