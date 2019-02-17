05e6ef9d0f1e02d4e984b17708818e3ef1da5a7e
Dan Moore <bitbucket@mooreds.com>
2019-02-17 17:01:12 +0000

    make sure we can run system tests

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,5 +1,8 @@
 require "test_helper"
+require 'selenium-webdriver'
+require 'capybara'
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  caps = Selenium::WebDriver::Remote::Capabilities.chrome("chromeOptions" => {"args" => %w(--headless --no-sandbox)})
+  driven_by :selenium, using: :chrome, screen_size: [1400, 1400], options: { desired_capabilities: caps }
 end
